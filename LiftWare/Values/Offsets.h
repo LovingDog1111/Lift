01319c3891a34ac5338e0263557033281d408d29
xuntf <xuntf@CYBERPOWER>
2026-01-11 17:56:50 -0500

    Cleanup 1.21.94

--- LiftWare/Values/Offsets.h
+++ LiftWare/Values/Offsets.h
@@ -9,13 +9,11 @@ public:
     inline static constexpr uintptr_t mcGame = 0xD0; //1.21.94
     inline static constexpr uintptr_t minecraft = 0xD0; //1.21.94
     inline static constexpr uintptr_t guiData = 0x5B8; //1.21.94
-    //inline static constexpr uintptr_t HMDState = 0x5A8; moved to vtables
     inline static constexpr uintptr_t BadRefDef = 0x330;
     inline static constexpr uintptr_t cameraPos = 0x664; //1.21.94
     inline static constexpr uintptr_t origin = 0x670;   //1.21.94
     inline static constexpr uintptr_t FovXMember = 0x1020; //1.21.94
     inline static constexpr uintptr_t FovYMember = 0x1034; //1.21.94
-
     inline static constexpr uintptr_t MouseGrabbed = 0x1B8; //1.21.94
     inline static constexpr uintptr_t GameMode = 0xA88; //1.21.94
     inline static constexpr uintptr_t Player = 0x8; //1.21.94
@@ -26,9 +24,6 @@ public:
     inline static constexpr uintptr_t MCTFBaseChar0 = 0xC2;
     inline static constexpr uintptr_t MCTFBaseChar1 = 0xA7;
     inline static constexpr uintptr_t PlayerMap = 0x1BC8;
-   // inline static constexpr uintptr_t Level = 0x1D8; //1.21.94
     inline static constexpr uintptr_t stateVector = 0x290; //1.21.94
-    //inline static constexpr uintptr_t aabbShape = 0x298; moved to components
-    //inline static constexpr uintptr_t rotation = 0x2A0; movded to components
     inline static const std::string version = "1.21.94";
 };
