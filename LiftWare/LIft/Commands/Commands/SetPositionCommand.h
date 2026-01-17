b6b325a4d4c5f6704786a7c8b313cb0a5e785853
LovingDog1111 <tcclarperlol@gmail.com>
2026-01-17 17:05:47 -0500

    Delete LiftWare/LIft directory

--- LiftWare/LIft/Commands/Commands/SetPositionCommand.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "CommandBase.h"
-
-class SetPositionCommand : public CommandBase {
-public:
-    SetPositionCommand();
-    bool execute(const std::vector<std::string>& args) override;
-};
